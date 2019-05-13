7bef48aec87aeaaa9299f956539a51c0d42e1daa
Joel Omari <mac@macs-MacBook-Pro-2.local>
2019-05-13 15:30:42 +0300

    adding templates2

--- tshirtshop/urls.py
+++ tshirtshop/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
 ]
